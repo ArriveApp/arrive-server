4bf592dfca57bc0bc4bf71fc672226cee0d81cad
Jered Philippon <jphilipp@thoughtworks.com>
2013-08-05 13:09:03 -0400

    Jered added cross origin headers to response for requests to the api from javascript

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :allow_cross_domain_access
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:user) { |u| u.permit(:name, :username, :school_id, :password, :password_confirmation, :firstname, :lastname) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :username, :school_id, :password, :password_confirmation, :firstname, :lastname) }
+  end
+
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST"
+    response.headers["Access-Control-Allow-Headers"] = "origin, Content-Type, X-Requested-With"
   end
 end
