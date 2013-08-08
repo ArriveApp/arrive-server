b6bfdeac68419eff589a7c0a7a3621a36d8247f0
shyamvala <shyam.vala@gmail.com>
2013-08-07 20:58:42 -0400

    Emily/Shyam - fixing a reports issue with dates

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -3,7 +3,9 @@ class CheckIn < ActiveRecord::Base
   belongs_to :course
 
   def self.search_by(from, to)
-    self.where('created_at >= ? AND created_at <= ?', from, to)
+    from_time = Time.strptime(from, '%m-%d-%Y').utc.beginning_of_day
+    to_time = Time.strptime(to,'%m-%d-%Y').utc.end_of_day
+    self.where('created_at >= ? AND created_at <= ?', from_time, to_time)
   end
 end
 
