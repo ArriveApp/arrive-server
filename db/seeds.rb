54dc8801c3ffb5fa9e0747858837181f66773e34
Cassie Shum <cassiend@gmail.com>
2013-08-09 16:11:50 -0400

    [Alex/Krystal/Cassie] - fix the db/seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # create test school
 hogwarts = School.create(name: "Hogwarts");
 winterfell = School.create(name: "Winterfell");
-nightengale = School.create(name: "Nightengale");
+nightingale = School.create(name: "Nightingale");
 
 # creating test users
 User.create(email: "testuser1@test.com", school_id: hogwarts.id, username: "harrypotter", password:"test", password_confirmation:"test", firstname: "Harry", lastname: "Potter");
@@ -12,6 +12,6 @@ User.create(email:"arya@test.com", school_id: winterfell.id, username: "aryastar
 User.create(email:"ned@test.com", school_id: winterfell.id, username: "nedstark", password:"test", password_confirmation:"test", firstname: "Ned", lastname: "Stark");
 User.create(email:"rob@test.com", school_id: winterfell.id, username: "robstark", password:"test", password_confirmation:"test", firstname: "Rob", lastname: "Stark");
 
-User.create(email:"someone@nightengale.edu", school_id: nightengale.id, username: "someone", password:"test", password_confirmation:"test", firstname: "somestudent", lastname: "yes");
+User.create(email:"someone@nightingale.edu", school_id: nightingale.id, username: "someone", password:"1234", password_confirmation:"test", firstname: "somestudent", lastname: "yes");
 
-User.create(email:"adamleystrnad@gnightingale.org", school_id: nightingale.id, username: "adamleystrnad", password:"1234", password_confirmation:"1234", firstname: "Alexandra", lastname: "Damley-Strnad");
+User.create(email:"adamleystrnad@nightingale.org", school_id: nightingale.id, username: "adamleystrnad", password:"1234", password_confirmation:"1234", firstname: "Alexandra", lastname: "Damley-Strnad");
