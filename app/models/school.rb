1838064193080cf669c5bb33135b172f08653ed5
Ryan Oglesby <ryan.oglesby08@gmail.com>
2013-08-10 18:14:44 -0500

    [Ryan] Basic styling for courses page.

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,6 @@ class School < ActiveRecord::Base
 	validates :name,  presence: true,
                     length: { minimum: 1, allow_blank: true }
 
-  has_many :courses
+  has_many :courses, -> {order :name}
   has_many :users
 end
