b0bc7660560d810962317fff62decb77636ce2f0
Jered Philippon <jphilipp@thoughtworks.com>
2013-08-12 12:24:29 -0400

    Jered added cross origin allowance to the API controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,8 +1,16 @@
 module Api
   class ApiController < ActionController::Base
     protect_from_forgery
+    before_filter :allow_cross_domain_access
 
     respond_to :json
+
+    def allow_cross_domain_access
+      response.headers["Access-Control-Allow-Origin"] = "*"
+      response.headers["Access-Control-Allow-Methods"] = "GET, POST"
+      response.headers["Access-Control-Allow-Headers"] = "origin, Content-Type, X-Requested-With"
+    end
+
   end
 
 end
