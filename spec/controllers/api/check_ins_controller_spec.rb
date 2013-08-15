2dd263b2fafb2042f770c1d03df881f73da5e1c9
Ryan Oglesby <ryan.oglesby08@gmail.com>
2013-08-14 20:12:52 -0400

    [Ryan] Accepting 'public' check ins. Check ins with pins.

--- spec/controllers/api/check_ins_controller_spec.rb
+++ spec/controllers/api/check_ins_controller_spec.rb
@@ -7,17 +7,18 @@ describe Api::CheckInsController do
       post :create, params.merge(check_in_params)
     end
 
-    let(:check_in) { double(CheckIn).as_null_object }
+    let(:school_id) { '1' }
     let(:course_id) { '2' }
     let(:user_id) { '3' }
-    let(:school_id) { '1' }
+
+    let(:user) { double(User, id: user_id) }
 
     before do
       stub_sign_in
-      CheckIn.stub(:create) { check_in }
-    end
 
-    let(:user) { double(User, id: user_id) }
+      CheckIn.stub(:create)
+      User.stub(:find) { user }
+    end
 
     context 'check in with no pin' do
       it 'creates a check in for the current user' do
@@ -30,12 +31,26 @@ describe Api::CheckInsController do
     end
 
     context 'check in with a pin' do
-      xit 'creates a check in for the user with the specified pin' do
-        User.should_receive(:find).with(pin: '1234') { user }
+      let(:pin) { '1234' }
+      let(:user_with_pin_id) { '4' }
+      let(:user_with_pin) { double(User, id: user_with_pin_id) }
 
-        CheckIn.should_receive(:create).with(course_id: course_id, user_id: user_id)
+      it 'creates a check in for the user with the specified pin' do
+        User.should_receive(:find_by).with(pin: pin) { user_with_pin }
 
-        post_to_create(pin: '1234')
+        CheckIn.should_receive(:create).with(course_id: course_id, user_id: user_with_pin_id, school_id: school_id)
+
+        post_to_create(pin: pin)
+      end
+
+      it 'is unauthorized and does not create a check in if the pin is not valid' do
+        User.should_receive(:find_by).with(pin: pin) { nil }
+
+        CheckIn.should_not_receive(:create)
+
+        post_to_create(pin: pin)
+
+        expect(response.status).to eq(401)
       end
     end
 
@@ -43,14 +58,6 @@ describe Api::CheckInsController do
       post_to_create
 
       expect(response.status).to eq(201)
-    end
-
-    it 'renders the check in as json' do
-      check_in.stub(:to_json) { {check_in: 'details'}.to_json }
-
-      post_to_create
-
-      expect(response.body).to eq({check_in: 'details'}.to_json)
     end
 
   end
