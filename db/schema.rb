c07d73f8240717362e36e0b1f66228f95c1a9eee
Ryan Oglesby <ryan.oglesby08@gmail.com>
2013-08-21 23:04:40 -0400

    Added an is_admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130821232726) do
+ActiveRecord::Schema.define(version: 20130822030204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20130821232726) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                default: "",   null: false
-    t.string   "encrypted_password",   default: "",   null: false
+    t.string   "email",                default: "",    null: false
+    t.string   "encrypted_password",   default: "",    null: false
     t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20130821232726) do
     t.string   "lastname"
     t.boolean  "is_teacher",           default: true
     t.string   "pin"
+    t.boolean  "is_admin",             default: false
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
