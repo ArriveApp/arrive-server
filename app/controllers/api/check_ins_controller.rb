422b3b272ce74fe7b6b812b7585a4572179eeb90
Pamela <pam.cdm@gmai.com>
2013-09-13 11:45:46 -0300

    [Pamela] Verify student has not checked in a period of  45 minutes

--- app/controllers/api/check_ins_controller.rb
+++ app/controllers/api/check_ins_controller.rb
@@ -1,19 +1,39 @@
+require 'pry'
+
 module Api
   class CheckInsController < ApiController
     def create
       pin = params[:pin]
       user = current_user
 
+
       if pin.present?
         user = User.find_by(pin: pin)
 
-        head :unauthorized and return if user.nil?
+        if user.nil?
+        render json: {success: false, error_message: "Invalid PIN" } , status: :unauthorized
+        return
+        end
+
       end
 
-      CheckIn.create(course_id: params[:course_id], user_id: user.id, school_id: params[:school_id])
+      time = Time.now   -  45*60
 
-      render json: {firstname: user.firstname}, status: :created
-    end
+      checkIn = CheckIn.where("course_id = :course_id AND user_id = :user_id AND school_id = :school_id AND created_at >= :time",
+                              {course_id: params[:course_id], user_id: user.id, school_id: params[:school_id], time: time})
+
+      if checkIn.count == 0
+        CheckIn.create(course_id: params[:course_id], user_id: user.id, school_id: params[:school_id])
+        render json: {success: true,firstname: user.firstname}, status: :created
+      else
+        course_id = params[:course_id]
+        courseToCheckIn = Course.find(course_id)
+        render json: {success: false, error_message: "Sorry! You have already checked in to " + courseToCheckIn.name } , status: :unauthorized
+
+      end
+
+  end
+
 
   end
 
