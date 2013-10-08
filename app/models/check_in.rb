7c7afc395d9bd4bef22166084dec913499003d90
Pamela <pam.cdm@gmai.com>
2013-10-07 21:11:04 -0300

    [Pamela] Add method to seach checkins by course

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,4 +1,4 @@
-class CheckIn < ActiveRecord::Base
+ class CheckIn < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
   belongs_to :school
@@ -11,6 +11,12 @@ class CheckIn < ActiveRecord::Base
     from_time = Time.strptime(from, '%m-%d-%Y').utc.beginning_of_day
     to_time = Time.strptime(to,'%m-%d-%Y').utc.end_of_day
     self.where('school_id = ? AND created_at >= ? AND created_at <= ?', school_id, from_time, to_time)
+  end
+
+  def self.search_by_course(from,to,school_id,course_id)
+    from_time = Time.strptime(from, '%m-%d-%Y').utc.beginning_of_day
+    to_time = Time.strptime(to,'%m-%d-%Y').utc.end_of_day
+    self.where('school_id = ?  AND course_id =?  AND created_at >= ? AND created_at <= ?', school_id, course_id, from_time, to_time)
   end
 end
 
