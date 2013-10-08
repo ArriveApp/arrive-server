06fd92defec6cf3de6d4e6fb73e1c4b9d953bc0b
Pamela <pam.cdm@gmai.com>
2013-10-07 21:08:56 -0300

    [Pamela] Add option to make report of students without checkins for a specific course

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,8 +5,28 @@ class ReportsController < ApplicationController
     school_id = params['school_id'].to_i
     @from = params['from'] || format_date(Date.today)
     @to = params['to'] || format_date(Date.today)
-    @check_ins = CheckIn.search_by(@from, @to, school_id)
+    @courses = Course.where(:school_id => school_id)
+    @report = params['report'].to_i
+    course_id = params['course']
+
+    @check_ins = []
+    @students_no_check_in = []
+
+    if @report == 1
+
+      @check_ins = CheckIn.search_by(@from, @to, school_id)
+    elsif @report == 2  and course_id!= ""
+
+      students_with_check_in =   CheckIn.search_by_course(@from, @to,school_id,course_id).select(:user_id).distinct
+      @students_no_check_in = User.where(is_teacher: false, is_admin:false).where.not(id: students_with_check_in)
+
+    end
+
   end
+
+
+
+
 
   private
 
